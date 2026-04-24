8ede06a56d360c43b510d63aec5ad01809272de3
TakacsLaszlo01 <takacs.laszlo.istvan.01.moricz@gmail.com>
2026-04-24 10:24:42 +0200

    Listázás a konzolon

--- Console/Program.cs
+++ Console/Program.cs
@@ -4,18 +4,36 @@ using System.Text;
 
 internal class Program
 {
+    private static ProductManager pm;
     static void Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
-        ProductManager pm = new();
+        pm = new();
 
+        Console.WriteLine("Utasítások:\nL - listázás\nI - beszúrás\nD - törlés\nU - szerkesztés\nX - kilépés");
+        string input = "L";
+        while(!input.Equals("X"))
+        {
+            switch(input)
+            {
+                case "L":
+                    ListProducts();
+                    break;
+                default: break;
+            }
+            input = Input("Adja meg a parancsot: ").ToUpper();
+        }
+    }
+    private static void ListProducts()
+    {
         List<Product> products = pm.GetProducts();
         int n = products.Count;
-
         for (int i = 0; i < n; i++)
-        {
             Console.WriteLine(products[i]);
-        }
-        
+    }
+    private static string Input(string input)
+    {
+        Console.Write(input);
+        return Console.ReadLine();
     }
 }
