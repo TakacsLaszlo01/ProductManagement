1bf23bf98bd16baed41183f46fb97218e8b5522d
TakacsLaszlo01 <takacs.laszlo.istvan.01.moricz@gmail.com>
2026-05-21 10:42:35 +0200

    Add files via upload

---
+++ Console/obj/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace ConsoleApp;
+using ProductsLibrary;
+using System.Text;
+
+internal class Program
+{
+    private static ProductManager pm;
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        pm = new();
+
+        Console.WriteLine("Utasítások:\nL - listázás\nI - beszúrás\nD - törlés\nU - szerkesztés\nX - kilépés");
+        string input = "L";
+        while(!input.Equals("X"))
+        {
+            switch(input)
+            {
+                case "L":
+                    ListProducts();
+                    break;
+                case "I":
+                    InsertProduct();
+                    break;
+                case "D":
+                    break;
+                case "U":
+                    break;
+                default:
+                    Console.WriteLine("Nem létező parancs!");
+                    break;
+            }
+            input = Input("Adja meg a parancsot: ").ToUpper();
+        }
+    }
+    private static void ListProducts()
+    {
+        List<Product> products = pm.GetProducts();
+        int n = products.Count;
+        for (int i = 0; i < n; i++)
+            Console.WriteLine(products[i]);
+    }
+    private static void InsertProduct()
+    {
+        string id, name;
+        double price;
+
+        id = Input("Adja meg a termék azonosítóját: ");
+        name = Input("Adja meg a termék nevét: ");
+        while (!double.TryParse(Input("Adja meg az árát: €"), out price))
+            Console.WriteLine("Pozitív számot adjon meg!");
+
+        pm.InsertProduct(id, name, price);
+    }
+    private static void UpdateProduct()
+    {
+
+    }
+    private static void DeleteProduct()
+    {
+
+    }
+    private static string Input(string input)
+    {
+        Console.Write(input);
+        return Console.ReadLine();
+    }
+}
